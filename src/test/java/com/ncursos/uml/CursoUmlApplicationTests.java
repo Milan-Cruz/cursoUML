5385897b980ce714ec00e066059c93d055d7f71e
Milan Rorchaque <milanmoreira@gmail.com>
2022-01-11 18:01:25 -0300

    apos o reset

---
+++ src/test/java/com/ncursos/uml/CursoUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncursos.uml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
